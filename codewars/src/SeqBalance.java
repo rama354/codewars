fd2d34f2939bcfa21977ffc0ea15e16ee7c52c32
Kartik <rama354@gmail.com>
2016-05-24 15:15:19 +0530

    D:\github\codewars\codewars\src

--- codewars/src/SeqBalance.java
+++ codewars/src/SeqBalance.java
@@ -12,81 +12,85 @@ public class SeqBalance {
 			return true;
 		else if (seqlen % 2 != 0)
 			return false;
-		else 
-		{
+		else {
 			int mididx = 0;
-			int[] markIdx=new int[seqlen];
-			
-			for (int i=0;i<seqlen;i++)
-				markIdx[i]=0;
-			
-			while (mididx<seqlen) 
+			int[] markIdx = new int[seqlen];
+			while(mididx<seqlen)
 			{
-				if (markIdx[mididx]==0)
-				{
+				if (markIdx[mididx]==0){
 					markIdx[mididx]=1;
-					if (s.charAt(mididx)=='(')
-						isGroupMatched(mididx,'(',')',s,markIdx);
-					else if (s.charAt(mididx)=='{')
-						isGroupMatched(mididx,'{','}',s,markIdx);
-					else if (s.charAt(mididx)=='[')
-						isGroupMatched(mididx,'[',']',s,markIdx);
+					if (s.charAt(mididx) == '(')
+						isGroupMatched(mididx, '(', ')', s, markIdx);
+					else if (s.charAt(mididx) == '{')
+						isGroupMatched(mididx, '{', '}', s, markIdx);
+					else if (s.charAt(mididx) == '[')
+						isGroupMatched(mididx, '[', ']', s, markIdx);
+					
 				}
 				
 				mididx++;
 			}
+			
+				
+			int opencount = 0, closecount = 0;
 
-			int opencount=0,closecount=0;
-			
-			for (int i=0;i<seqlen;i++)
-				 if (markIdx[i]==1)
-					 opencount++;
-				 else if (markIdx[i]==2)
-					 closecount++;
-			
-			if (opencount==closecount)
+			for (int i = 0; i < seqlen; i++)
+				if (markIdx[i] == 1)
+					opencount++;
+				else if (markIdx[i] == 2)
+					closecount++;
+
+			if (opencount == closecount)
 				return true;
 			else
 				return false;
 		}
 	}
 
-	private static int isGroupMatched(int mididx, char c, char d, String s, int[] markIdx) {
-		
-		int openidx=0,closeidx = 0;
-		
-		if (mididx==-1)
-			return mididx;		
-		else
+	private static int isGroupMatched(int mididx, char c, char d, String s,
+			int[] markIdx) {
+
+		int openidx = 0, closeidx = 0;
+
+		if (mididx == -1)
+			return mididx;
+		else 
 		{
-			markIdx[mididx]=1;
-					
+			markIdx[mididx] = 1;
+	
+			/*if (s.charAt(mididx+1) == '(')
+				isGroupMatched(mididx+1, '(', ')', s, markIdx);
+			else if (s.charAt(mididx+1) == '{')
+				isGroupMatched(mididx+1, '{', '}', s, markIdx);
+			else if (s.charAt(mididx+1) == '[')
+				isGroupMatched(mididx+1, '[', ']', s, markIdx);*/
+		
 			isGroupMatched(s.indexOf(c, mididx + 1), c, d, s, markIdx);
-			
-			if (mididx + 1<s.length())
-				if (s.charAt(mididx + 1)==d && markIdx[mididx + 1]==0){
-					markIdx[mididx + 1]=2;
+
+			if (mididx + 1 < s.length()){
+				if (s.charAt(mididx + 1) == d && markIdx[mididx + 1] == 0) {
+					markIdx[mididx + 1] = 2;
 					return mididx + 1;
-			}
-			
-			openidx=mididx+1;
-			while(true)
-			{
-				closeidx=s.indexOf(d,openidx+1);
-				if (closeidx==-1)
-					break;
-				
-				if (markIdx[closeidx]==0){
-					markIdx[closeidx] = 2;
-					break;
 				}
-				openidx=closeidx;
+				
 			}
 			
-			
-			return closeidx;	
+			openidx = mididx + 1;
+			while (true) 
+			{
+ 				closeidx = s.indexOf(d, openidx + 1);
+ 				if (closeidx == -1)
+ 					break;
+ 
+ 				if (markIdx[closeidx] == 0 && (closeidx-mididx)%2!=0) {
+ 					markIdx[closeidx] = 2;
+ 					break;
+ 				}
+ 				openidx = closeidx;
+			 }
+			return closeidx;
 		}
-						
+
 	}
 
 	public static void main(String[] args) {
